e112affb4e797b9dbf942390cce360c25e0fd81b
AudFil <audric_filipino@dlsu.edu.ph>
2025-10-09 21:12:02 +0800

    feat: added format as decimal and number

--- tests/eventInput.test.js
+++ tests/eventInput.test.js
@@ -1,13 +1,17 @@
 const {
-    checkStringInput
+    checkStringInput,
+    formatAsDecimal,
+    formatAsNumber
 } = require('../public/js/event-tracker-form.js');
 
 describe('check if contains blacklisted characters', () => {
     it('check for ~', () => {
         //arrange
         const input = 'cha~hing';
+
         //act
         const result = checkStringInput(input);
+
         //expect
         expect(result).toEqual(true);
     });
@@ -15,8 +19,10 @@ describe('check if contains blacklisted characters', () => {
     it('check for ` #', () => {
         //arrange
         const input = 'cha`` #hing';
+
         //act
         const result = checkStringInput(input);
+
         //expect
         expect(result).toEqual(true);
     });
@@ -24,8 +30,10 @@ describe('check if contains blacklisted characters', () => {
     it('check for $ \\', () => {
         //arrange
         const input = 'cha$hing\\';
+
         //act
         const result = checkStringInput(input);
+
         //expect
         expect(result).toEqual(true);
     });
@@ -33,9 +41,81 @@ describe('check if contains blacklisted characters', () => {
     it('check for no blacklist', () => {
         //arrange
         const input = 'chaching';
+
         //act
         const result = checkStringInput(input);
+
         //expect
         expect(result).toEqual(false);
+    });
+});
+
+describe('format decimal ', () => {
+    it('round up', () => {
+        //arrange
+        const input = '19.875';
+
+        //act
+        const result = formatAsDecimal(input);
+
+        //expect
+        expect(result).toEqual('19.88');
+    });
+
+    it('format with two decimal places', () => {
+        //arrange
+        const input = '19';
+
+        //act
+        const result = formatAsDecimal(input);
+
+        //expect
+        expect(result).toEqual('19.00');
+    });
+
+    it('round down', () => {
+        //arrange
+        const input = '19.874';
+
+        //act
+        const result = formatAsDecimal(input);
+
+        //expect
+        expect(result).toEqual('19.87');
+    });
+});
+
+describe('format numbers with commas', () => {
+    it('format decimal', () => {
+        //arrange
+        const input = '19,875';
+
+        //act
+        const result = formatAsNumber(input);
+
+        //expect
+        expect(result).toEqual(19875);
+    });
+
+    it('format float', () => {
+        //arrange
+        const input = '19,207.1234';
+
+        //act
+        const result = formatAsNumber(input);
+
+        //expect
+        expect(result).toEqual(19207.1234);
+    });
+
+    it('no comma', () => {
+        //arrange
+        const input = '874';
+
+        //act
+        const result = formatAsNumber(input);
+
+        //expect
+        expect(result).toEqual(874);
     });
 });
