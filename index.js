14c69935a34f52372813d9ff853225af75922b09
ChingManWong <ching_man_wong@dlsu.edu.ph>
2025-11-05 22:28:59 +0800

    feat: added frontend client-side js

--- index.js
+++ index.js
@@ -2,18 +2,37 @@ require('dotenv').config();
 hostname = process.env.HOSTNAME
 port = process.env.PORT
 
+//import the necessary modules
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-var handlebars_helper; //in the future, require(`./views hbs helper.js`) or something
-var routes;            //same as abpve
+const handlebars_helper = require(`./views/hbs-helper.js`);
+const routes = require('./routes/routes.js');
 const mongoose = require('mongoose');
-var db;                //same here; !! change to const
+const db = require('./models/db.js');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const app = express();
 
-
+//parse incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: false }));
+//parse incoming json payload
+app.use(express.json());
+//set the file path containing the static assets
+app.use(express.static(path.join(__dirname, 'public')));
+//set the session middleware
+app.use(
+    session({
+        secret: 'balai-yllana',
+        resave: 'false',
+        saveUninitialized: 'false',
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    })
+);
+//set hbs as the view engine
+app.set('view engine', 'hbs');
+//set the file path containing the hbs files
+app.set('views', path.join(__dirname, 'views'));
 //check if current user has logged in
 app.use('/', (req, res, next) => {
     if (
