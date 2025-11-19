76cb18bb43c1d8e711dabf6e0acd88bf61093f56
aaaaandreiii <andrei.balingit.alab1@gmail.com>
2025-11-19 12:04:34 +0800

    TODO: sanitization and validation of inputs

---
+++ helpers/validation.js
@@ -0,0 +1,6 @@
+// TODO:
+    // sanitizeString(str) to trims, strips dangerous characters.
+    // isValidUsername(username) for length + allowed chars.
+    // isValidPhone(contactNum) for regex for digits, length limit.
+    // isValidName(name) for letters / spaces, length.
+    // maybe isValidDate, sanitizeNumber, etc
