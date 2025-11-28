639e1f00c56848f4ac47ae397ca38fb301d3f44a
aaaaandreiii <andrei.balingit.alab1@gmail.com>
2025-11-29 07:01:32 +0800

    fix: prettier

--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -24,7 +24,7 @@ const eventController = {
         $match: {
           status: {
             // $ne: "cancelled",
-            $in: ["reserved", "booked", "finished"]
+            $in: ["reserved", "booked", "finished"],
           },
           eventDate: {
             $gte: today,
